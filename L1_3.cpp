d7902febb5fef83afd98b57dc773f8a14d36b6fb
kmshawon <kmshawon01850160659@gmail.com>
2024-05-27 05:27:48 +0600

    problem 3

---
+++ L1_3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    reverse(a.begin(),a.end());
+    if(b==a){
+            cout<<"YES\n";
+    }
+    else{
+            cout<<"NO\n";
+    }
+}
